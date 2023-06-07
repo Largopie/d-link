fd41c94e9f6501b821a6e802c36e834b373f90d7
Largopie <106071687+Largopie@users.noreply.github.com>
2023-06-07 13:22:12 +0900

    헤더 및 메뉴 레이아웃 CSS 리팩토링

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -4,19 +4,49 @@ import styled from "styled-components";
 import Content from "@pages/content";
 
 const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   width: 100vw;
   height: 100vh;
-  background-color: #F8FDFF;
+  background-color: #f8fdff;
 `;
 
+const HeaderStyle = styled.div`
+  position: absolute;
+  top: 0px;
+  width: 100vw;
+  height: 80px;
+  background-color: #048ad7;
+`;
 
+const MenuStyle = styled.div`
+  position: absolute;
+  top: 80px;
+  width: 100vw;
+  height: 48px;
+  background-color: #ffffff;
+  box-shadow: 0px 3px 6px #4E4E4E29;
+`;
+
+const SubContainer = styled.div`
+  max-width: 960px;
+  height: 100vh;
+  
+  background-color: black;
+`;
 
 const MainPage = () => {
   return (
     <MainContainer>
-      <Header />
+      <HeaderStyle />
+      <MenuStyle />
+      {/* <SubContainer>
+        123
+      </SubContainer> */}
+      {/* <Header />
       <Menu />
-      <Content />
+      <Content /> */}
     </MainContainer>
   );
 };
